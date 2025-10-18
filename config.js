db4ca68b190866dccf6d4ee866003cc5d6b493ad
SACHIN MR <166192886+Sachin-mr@users.noreply.github.com>
2025-10-18 14:50:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Create a new file named config.js and add your Gemini API key
-const GEMINI_API_KEY = "AIzaSyDlSCzDL5r5mCqFiEWkZ4gI_90lOPhR6gU";
+const GEMINI_API_KEY = AIzaSyDlSCzDL5r5mCqFiEWkZ4gI_90lOPhR6gU;
